8c598afd1bfbc9697d97265de3b4317fc47e141c
Prateeklk <prateeklk24@gmail.com>
2025-12-03 23:07:50 -0500

    unit test cases updated

---
+++ tests/test_operation.py
@@ -0,0 +1,17 @@
+from src.maths_opeartion import add, sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(7,5) == 13
+
+def test_sub():
+    assert sub(3,2) == 1
+    assert sub(4,6) == -2
+
+
+
+
+
+
+
+
