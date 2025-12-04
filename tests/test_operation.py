64abe6d758f969cc309e81ccd279d1b2860a64c8
Prateek Lokesh Kumbar <68516313+Prateeklk@users.noreply.github.com>
2025-12-03 23:22:31 -0500

    Correct expected result for add function test
    Fix incorrect expected result in add test case.

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2,7 +2,7 @@ from src.maths_opeartion import add, sub
 
 def test_add():
     assert add(2,3) == 5
-    assert add(7,5) == 13
+    assert add(7,5) == 12
 
 def test_sub():
     assert sub(3,2) == 1
